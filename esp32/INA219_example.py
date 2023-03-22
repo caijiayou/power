f08a05776d07c649963df935995bd7ebda7a03ea
caijiayou <ab881027@gmail.com>
2023-03-22 11:30:19 +0800

    update ina219_example

--- esp32/INA219_example.py
+++ esp32/INA219_example.py
@@ -2,16 +2,12 @@ from machine import Pin, I2C
 from ina219 import INA219
 from logging import INFO
 
-def myina219():
-    SHUNT_OHMS = 0.1
+SHUNT_OHMS = 0.1
 
-    i2c = I2C(-1, Pin(22), Pin(21))
-    ina = INA219(SHUNT_OHMS, i2c, log_level=INFO)
-    ina.configure()
-    lst=[]
-    lst.append(ina.voltage, ina.current, ina.power)
-    return lst
+i2c = I2C(-1, Pin(22), Pin(21))
+ina = INA219(SHUNT_OHMS, i2c, log_level=INFO)
+ina.configure()
 
-if __name__=='__main__':
-    lst = myina219()
-    print(lst)
+print("Bus Voltage: %.3f V" % ina.voltage())
+print("Current: %.3f mA" % ina.current())
+print("Power: %.3f mW" % ina.power())
